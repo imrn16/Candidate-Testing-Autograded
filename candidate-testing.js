2dfb76627da45941aa2fd7961cc46adeee2735ca
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-07-02 12:07:04 -0500

    modified:   candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -38,7 +38,7 @@ function askQuestion() {
 		candidateAnswers.push(input.question(questions[i]))
 	}
 	
-}
+} 
 
 function gradeQuiz(candidateAnswers) {
 	// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
